6fbb0f9b2558f13f679c9a9325696c7ce01bc32b
Justin <jmp86@yahoo.com>
2021-09-04 17:17:37 -1000

    commit tree dd53588c7b76075c9878d34fcc79a3b4fd3169fe

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+    attr_accessor :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        backers =ProjectBacker.all.select {|backer| backer.project == self}
+        backers.map {|b| b.backer}
+    end
+
+end
